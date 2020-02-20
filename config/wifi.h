874e59771d6a6cc024c14349d2ccfa595ac978aa
jramirez <josepichu@gmail.com>
2020-02-21 00:11:35 +0100

    Update wifi.h

--- config/wifi.h
+++ config/wifi.h
@@ -1,5 +1,5 @@
-const char* ssid     = "Ziggo3564757";
-const char* password = "cv7xsDdzPgj6+";
+const char* ssid     = "";
+const char* password = "";
 const char* hostname = "esp8266"; // hostname for multicast DNS
 
 IPAddress ip(192, 168, 178, 100);
